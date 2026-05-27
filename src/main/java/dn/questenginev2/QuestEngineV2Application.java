76633b2c6df3af87a64b5102560cb08976a5aa63
Denis Neverov <denis.neverov.dev@gmail.com>
2026-05-27 11:42:21 +0300

    QEv2: init

---
+++ src/main/java/dn/questenginev2/QuestEngineV2Application.java
@@ -0,0 +1,13 @@
+package dn.questenginev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestEngineV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestEngineV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/dn/questenginev2/QuestEngineV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dn.questenginev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestEngineV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
